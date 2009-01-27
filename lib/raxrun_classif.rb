da3376e38b76887f9f66dc76a17052237459b200
Your Name <you@example.com>
2009-01-27 14:30:01 +0100

    wrote intelligent 'batch' scripts for classifier

---
+++ lib/raxrun_classif.rb
@@ -0,0 +1,47 @@
+
+
+if ARGV.length < 6
+    puts "usage: raxrun <dataset> <model> <seq_start> <seq_end> <gap_start> <gap_end>"
+    throw "missing arguments"
+end
+
+$dataset = ARGV[0]
+$model = ARGV[1]
+$seq_start = ARGV[2].to_i
+$seq_end = ARGV[3].to_i
+$gap_start = ARGV[4].to_i
+$gap_end = ARGV[5].to_i
+
+
+$raxml_bin = "~/src/raxml720/raxmlHPC-PTHREADS"
+$misc_param = "-T 4 -x 1234"
+$algo = "y"
+
+
+$degalign_dir = "~/data/degen_alignments"
+$tree_dir = "~/data/redtree"
+$seq_pad = 4;
+
+
+
+$seq_start.upto($seq_end) do |seq|
+    seq = "#{"0" * [0,$seq_pad - seq.to_s.length].max}#{seq}"
+
+    $gap_end.step($gap_start, -10) do |gap|
+    #left pad sequence number
+    
+        tree = "#{$tree_dir}/RAxML_bipartitions.#{$dataset}.BEST.WITH_#{seq}"
+        align = "#{$degalign_dir}/#{$dataset}_#{seq}_#{gap}"
+        name = "#{$dataset}_#{$model}_#{$algo}_#{seq}_#{gap}"
+
+        num_bs = 100
+
+        cmd = "#{$raxml_bin} #{$misc_param} -f #{$algo} -m #{$model} -t #{tree} -s #{align} -n #{name} -\# #{num_bs}"
+
+        puts cmd
+
+
+    end
+end
+
+
---
+++ lib/raxrun_classif_align.rb
@@ -0,0 +1,66 @@
+
+
+if ARGV.length < 5
+    puts "usage: raxrun <dataset> <model> <seq_start> <seq_end> <len>"
+    throw "missing arguments"
+end
+
+$dataset = ARGV[0]
+$model = ARGV[1]
+$seq_start = ARGV[2].to_i
+$seq_end = ARGV[3].to_i
+$subseq_len = ARGV[4].to_i
+
+$raxml_bin = "~/src/raxml720/raxmlHPC"
+$misc_param = "-x 1234"
+
+$subseq_dir = "~/data/subseq_alignments"
+$tree_dir = "~/data/redtree"
+$seq_pad = 4
+
+$algo = "q"
+
+def strtime
+    return Time.now.strftime( "%Y_%m_%d__%H_%M_%S" );
+end
+$prot_name = strtime + ".txt"
+$protverbose_name = strtime + ".v.txt"
+
+$prot = File.new( $prot_name, "w" );
+
+$prot.puts "ARGV: '#{ARGV.join(" ")}'"
+
+$protverbose = File.new( $protverbose_name, "w" );
+$seq_start.upto($seq_end) do |seq|
+    seq = "#{"0" * [0,$seq_pad - seq.to_s.length].max}#{seq}"
+
+    tree = "#{$tree_dir}/RAxML_bipartitions.#{$dataset}.BEST.WITH_#{seq}"
+    align = "#{$subseq_dir}/#{$dataset}_#{seq}_#{$subseq_len}"
+    num_bs = 100
+    name = "#{$dataset}_#{$model}_#{$algo}_#{seq}_#{$subseq_len}"
+
+    cmd = "#{$raxml_bin} #{$misc_param} -f #{$algo} -m #{$model} -t #{tree} -s #{align} -n #{name} -\# #{num_bs}"
+    
+    $prot.puts "=========\n#{Time.now.asctime}\ncwd: #{`pwd`.strip}\ncmd: #{cmd}"
+    $protverbose.puts "=========\n#{Time.now.asctime}\ncwd: #{`pwd`.strip}\nhost: #{`hostname -f`.strip}\ncmd: #{cmd}\n>>>>>>>>"
+
+    if true
+        IO.popen(cmd) do |h|
+            while line = h.gets
+                $stdout.puts line
+                $protverbose.puts line
+                $protverbose.flush
+            end
+        end
+
+    else
+        puts cmd
+    end
+    $protverbose.puts "<<<<<<<<"
+    $protverbose.flush
+    $prot.flush
+    
+end
+
+$prot.close
+$protverbose.close
