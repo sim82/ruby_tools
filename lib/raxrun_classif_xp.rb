bcfb3153d5f65d2c78a6d3d97cd4af71db9aabf7
Your Name <you@example.com>
2009-02-11 12:24:58 +0100

    changes

--- lib/raxrun_classif_xp.rb
+++ lib/raxrun_classif_xp.rb
@@ -1,8 +1,8 @@
 
 
 if ARGV.length < 6
-    puts "usage: raxrun <dataset> <model> <seq_start> <seq_end> <gap_start> <gap_end>"
-    throw "missing arguments"
+	puts "usage: raxrun <dataset> <model> <seq_start> <seq_end> <gap_start> <gap_end>"
+	throw "missing arguments"
 end
 
 $dataset = ARGV[0]
@@ -28,29 +28,29 @@ $tree_dir = "~/data/redtree"
 $seq_pad = 4;
 
 def strtime
-    return Time.now.strftime( "%Y_%m_%d__%H_%M_%S" );
+	return Time.now.strftime( "%Y_%m_%d__%H_%M_%S" );
 end
 $prot_name = strtime + "_#{Process.pid}.txt"
 $protverbose_name = strtime + "_#{Process.pid}.v.txt"
 
 $seq_start.upto($seq_end) do |seq|
-    seq = "#{"0" * [0,$seq_pad - seq.to_s.length].max}#{seq}"
-    $gap_end.step($gap_start, -10) do |gap|
-    #left pad sequence number
-        
+	seq = "#{"0" * [0,$seq_pad - seq.to_s.length].max}#{seq}"
+	$gap_end.step($gap_start, -10) do |gap|
+	#left pad sequence number
+		
 
-        tree = "#{$tree_dir}/RAxML_bipartitions.#{$dataset}.BEST.WITH_#{seq}"
-        align = "#{$degalign_dir}/#{$dataset}_#{seq}_#{gap}"
-        name = "#{$dataset}_#{$model}_#{$algo}_#{seq}_#{gap}"
+		tree = "#{$tree_dir}/RAxML_bipartitions.#{$dataset}.BEST.WITH_#{seq}"
+		align = "#{$degalign_dir}/#{$dataset}_#{seq}_#{gap}"
+		name = "#{$dataset}_#{$model}_#{$algo}_#{seq}_#{gap}"
 
 	$prot.puts "=========\n#{Time.now.asctime}\ncwd: #{`pwd`.strip}\ncmd: #{cmd}"
 	$protverbose.puts "=========\n#{Time.now.asctime}\ncwd: #{`pwd`.strip}\nhost: #{`hostname -f`.strip}\ncmd: #{cmd}\n>>>>>>>>"
 
-        num_bs = 100
+		num_bs = 100
 
-        cmd = "#{$raxml_bin} #{$misc_param} -f #{$algo} -m #{$model} -t #{tree} -s #{align} -n #{name} -\# #{num_bs}"
+		cmd = "#{$raxml_bin} #{$misc_param} -f #{$algo} -m #{$model} -t #{tree} -s #{align} -n #{name} -\# #{num_bs}"
 
-		if true
+		if false
 			IO.popen(cmd) do |h|
 				while line = h.gets
 					$stdout.puts line
@@ -67,10 +67,10 @@ $seq_start.upto($seq_end) do |seq|
 		$protverbose.flush
 		$prot.flush
 
-        #puts cmd
+		#puts cmd
 
 
-    end
+	end
 end
 
 
